2224e7343e09e435c3f85f8d91970385e9ed9c09
Boombaastical <anjuna1234@gmail.com>
2026-05-05 20:01:55 +0200

    Small fix

--- SerialPrograms/Source/PokemonBDSP/Programs/AutoStory/StorySegments/PokemonBDSP_AutoStory_Segment_05.cpp
+++ SerialPrograms/Source/PokemonBDSP/Programs/AutoStory/StorySegments/PokemonBDSP_AutoStory_Segment_05.cpp
@@ -59,6 +59,26 @@ void AutoStory_Checkpoint_05::run_checkpoint(
     checkpoint_05(env, context, options, stats);
 }
 
+static void move_back_after_unexpected_battle(
+    VideoStream& stream,
+    ProControllerContext& context,
+    const std::string& trainerid
+){
+    context.wait_for_all_requests();
+    pbf_wait(context, 2000ms);
+
+    if (trainerid == "route_213_tuber_chelsea") {
+
+        pbf_move_left_joystick(context, {-1, 0}, 800ms, 100ms); // 4+
+        pbf_move_left_joystick(context, {0, +1}, 1000ms, 100ms); // 4+
+        pbf_move_left_joystick(context, {0, -1}, 800ms, 100ms); // 5
+    } else if (trainerid == "route_213_tuber_jared") {
+        // Just need to wait, nothing else
+    } else if (trainerid == "route_213_beauty_cindy") {
+        pbf_move_left_joystick(context, {0, -1}, 400ms, 100ms); // 1+
+    }
+}
+
 static bool handle_battle(
     VideoStream& stream,
     ProControllerContext& context,
@@ -87,6 +107,30 @@ static bool handle_battle(
         context.wait_for_all_requests();
         pbf_mash_button(context, BUTTON_B, 2000ms);
         stream.log("handle_battle_pastoria_clint: transition confirmed.");
+        return true;
+    } else if (
+        trainerid == "route_213_tuber_chelsea" or
+        trainerid == "route_213_tuber_jared" or
+        trainerid == "route_213_beauty_cindy"
+    ) {
+        // Select Razor leaf
+        pbf_press_dpad(context, DPAD_UP, 280ms, 200ms);
+
+        BlackScreenOverWatcher black_screen(COLOR_RED, {0.1, 0.1, 0.8, 0.8});
+        int ret = run_until<ProControllerContext>(
+            stream, context,
+            [](ProControllerContext& context){
+                pbf_mash_button(context, BUTTON_A, 200000ms);
+            },
+            {{black_screen}}
+        );
+        if (ret < 0){
+            stream.log("route_213_tuber_chelsea / jared: black screen not detected!", COLOR_RED);
+            return false;
+        }
+        context.wait_for_all_requests();
+        pbf_mash_button(context, BUTTON_B, 2000ms);
+        move_back_after_unexpected_battle(stream, context, trainerid);
         return true;
     }
     return false;
@@ -283,6 +327,104 @@ static bool go_through_house_east_pastoria(
     return true;
 }
 
+static bool go_through_route_213_chasing(
+    VideoStream& stream,
+    ProControllerContext& context
+){
+    DpadState dpad;
+    context.wait_for_all_requests();
+    pbf_wait(context, 2000ms);
+    context.wait_for_all_requests();
+    stream.log("go_through_route_213_chasing: continuing the drama");
+
+    pbf_move_left_joystick(context, {+1, 0}, 2000ms, 100ms); // 12
+    activate_repel(stream, context);
+    context.wait_for_all_requests();
+    pbf_wait(context, 1000ms);
+    pbf_move_left_joystick(context, {+1, 0}, 3600ms, 100ms); // 18+
+
+    dpad.last_dir = DPAD_RIGHT;
+    repeat_dpad(context, dpad, DPAD_LEFT, 80ms, 300ms, 5);
+
+    MarkDetector beauty_cindy_fight(stream.overlay(), {0.1, 0.0, 0.8, 0.9});
+    ret = run_until<ProControllerContext>(
+        stream, context,
+        [](ProControllerContext& context){
+            pbf_move_left_joystick(context, {0, -1}, 5000ms, 100ms); // 15+
+        },
+        {{beauty_cindy_fight}}
+    );
+    if (ret == 0){
+        BlackScreenOverWatcher black_screen(COLOR_RED, {0.1, 0.1, 0.8, 0.8});
+        ret = run_until<ProControllerContext>(
+            stream, context,
+            [](ProControllerContext& context){
+                pbf_mash_button(context, BUTTON_A, 200000ms);
+            },
+            {{black_screen}}
+        );
+        if (ret < 0){
+            stream.log("go_through_route_213_beauty_cindy: Didn't detect battle starting!", COLOR_RED);
+            return false;
+        } else if (ret == 0) {
+            context.wait_for_all_requests();
+            stream.log("go_through_route_213_beauty_cindy: Starting battle.");
+            handle_battle(stream, context, "route_213_beauty_cindy");
+            context.wait_for_all_requests();
+            pbf_wait(context, 3000ms);
+        }
+    }
+    context.wait_for_all_requests();
+    pbf_wait(2000ms);
+
+    pbf_move_left_joystick(context, {-1, 0}, 600ms, 100ms); // 4
+    pbf_move_left_joystick(context, {0, -1}, 1400ms, 100ms); // 9
+    pbf_move_left_joystick(context, {+1, 0}, 1600ms, 100ms); // 8+
+    pbf_move_left_joystick(context, {0, +1}, 400ms, 100ms); // 2+
+    pbf_move_left_joystick(context, {+1, 0}, 600ms, 100ms); // 4
+    pbf_move_left_joystick(context, {0, +1}, 600ms, 100ms); // 3+
+    pbf_move_left_joystick(context, {-1, 0}, 600ms, 100ms); // 3+
+
+    dpad.last_dir = DPAD_LEFT;
+    repeat_dpad(context, dpad, DPAD_LEFT, 80ms, 300ms, 2);
+    pbf_move_left_joystick(context, {-1, 0}, 600ms, 100ms); // 3+
+
+    context.wait_for_all_requests();
+
+    ShortDialogWatcher talk_to_grunt(COLOR_BLUE);
+    ret = run_until<ProControllerContext>(
+        stream, context,
+        [](ProControllerContext& context){
+            ssf_mash_button(context, BUTTON_A, 20000ms);
+            ssf_
+        },
+        {{talk_to_grunt_1}}
+    );
+    if (ret < 0) {
+        stream.log("pastoria_city_drama_talk_to_grunt_2: dialog box not detected!", COLOR_RED);
+    }
+    if (ret == 0){
+        stream.log("pastoria_city_drama_talk_to_grunt_2: talking to grunt!");
+        
+        BlackScreenOverWatcher black_screen(COLOR_RED, {0.1, 0.1, 0.8, 0.8});
+        int ret1 = run_until<ProControllerContext>(
+            stream, context,
+            [](ProControllerContext& context){
+                pbf_mash_button(context, BUTTON_B, 200000ms);
+            },
+            {{black_screen}}
+        );
+        if (ret1 < 0){
+            stream.log("pastoria_city_drama_talk_to_clint: Didn't detect start of battle with Clint!", COLOR_RED);
+            return false;
+        }
+        stream.log("pastoria_city_drama_talk_to_clint: battling Clint.");
+        handle_battle(stream, context, "pastoria_clint");
+        pbf_mash_button(context, BUTTON_B, 3000ms);
+    };
+
+}
+
 void checkpoint_05(
     SingleSwitchProgramEnvironment& env,
     ProControllerContext& context,
@@ -306,7 +448,9 @@ void checkpoint_05(
             if (!go_through_house_east_pastoria(env.console, context)){
                 OperationFailedException::fire(ErrorReport::SEND_ERROR_REPORT, "go_through_house_east_pastoria: transition not detected.", env.console);
             }
-            
+            if (!go_through_route_213_chasing(env.console, context)){
+                OperationFailedException::fire(ErrorReport::SEND_ERROR_REPORT, "go_through_route_213_chasing: transition not detected.", env.console);
+            }
         }
     );
 }
