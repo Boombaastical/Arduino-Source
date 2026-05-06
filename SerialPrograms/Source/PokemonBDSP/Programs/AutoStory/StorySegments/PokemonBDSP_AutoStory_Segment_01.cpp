9bb783285ea2407f6d0eec9f2060d0768d4a83b5
GumGumDragoon <51164935+GumGumDragoon@users.noreply.github.com>
2026-05-06 13:42:15 +0100

    Updated Segment 01 with path coding for Gym Puzzle 01

--- SerialPrograms/Source/PokemonBDSP/Programs/AutoStory/StorySegments/PokemonBDSP_AutoStory_Segment_01.cpp
+++ SerialPrograms/Source/PokemonBDSP/Programs/AutoStory/StorySegments/PokemonBDSP_AutoStory_Segment_01.cpp
@@ -5,13 +5,68 @@
  *
  */
 
+#include "CommonTools/Async/InferenceRoutines.h"
+#include "NintendoSwitch/Commands/NintendoSwitch_Commands_PushButtons.h"
+#include "PokemonBDSP/Inference/PokemonBDSP_DialogDetector.h"
 #include "../PokemonBDSP_AutoStoryTools.h"
 #include "PokemonBDSP_AutoStory_Segment_01.h"
+
+using namespace std::chrono_literals;
 
 namespace PokemonAutomation{
 namespace NintendoSwitch{
 namespace PokemonBDSP{
 
+// ---------------------------------------------------------------------------
+//  Segment 1 Part ? Gym navigation
+//  Navigates the player through the first Gym in Oreburgh City and completes the Gym battle.
+// ---------------------------------------------------------------------------
+
+static void gym_Puzzle_01(VideoStream& stream, ProControllerContext& context){
+
+    //Upon entering the Gym.
+
+    for(int i=0; i < 3; i++){
+        pbf_press_dpad(context, DPAD_UP, 80ms, 300ms);
+    }
+
+    for(int i=0; i < 4; i++){
+        pbf_press_dpad(context, DPAD_LEFT, 80ms, 300ms);
+    }
+
+    for(int i=0; i < 5; i++){
+        pbf_press_dpad(context, DPAD_UP, 80ms, 300ms);
+    }
+
+    for(int i=0; i < 8; i++){
+        pbf_press_dpad(context, DPAD_RIGHT, 80ms, 300ms);
+    }
+
+    for(int i=0; i < 11; i++){
+        pbf_press_dpad(context, DPAD_UP, 80ms, 300ms);
+    }
+
+    for(int i=0; i < 5; i++){
+        pbf_press_dpad(context, DPAD_LEFT, 80ms, 300ms);
+    }
+
+    for(int i=0; i < 4; i++){
+        pbf_press_dpad(context, DPAD_UP, 80ms, 300ms);
+    }
+
+    //Button presses to initiate the Gym battle.
+
+    //Battle automation code.
+
+    //Button presses after battle with small cutscene of badge being put in case between dialogue boxes.
+
+    for(int i=0; i < 21; i++){
+        pbf_press_dpad(context, DPAD_DOWN, 80ms, 300ms);
+    }
+
+    //Exits Buiilding
+    pbf_wait(context, 1000ms);
+}
 
 std::string AutoStory_Segment_01::name()       const{ return "01: Starter Received to First Badge"; }
 std::string AutoStory_Segment_01::start_text() const{ return "Start: Starter Pokemon chosen and received."; }
@@ -58,6 +113,9 @@ void checkpoint_01(
     checkpoint_reattempt_loop(env, context, options.notif_status_update, stats,
         [&](size_t /*attempt*/){
             // TODO: implement Segment 01 gameplay logic
+
+            //#. Oreburgh Gym navigation and battle
+            gym_Puzzle_01(env.console, context);
         }
     );
 }
